4ec3353be9b30c4caaf8834728c171ffcdd6f5e5
gsaintskad <gsaintskad@gmail.com>
2024-12-26 01:56:06 +0100

    theming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import { GraphBuilder } from "./GraphBuilder/GraphBuilder.tsx";
 import store from "@/redux/store.ts";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@/components/shadcnUI/ThemeProvider.tsx";
+import {ModeToggle} from "@/components/shadcnUI/ModeToggle.tsx";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div
-        className={"flex items-center justify-center bg-gray-100 h-full w-full"}
-      >
-
-        <GraphBuilder style={{ height: "70%", width: "70%" }} />
-      </div>
-    </Provider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Provider store={store}>
+        <div
+          className={
+            "flex bg-background text-foreground h-full w-full"
+          }
+        >
+          {/*<GraphBuilder style={{ height: "70%", width: "70%" }} />*/}
+        </div>
+        <ModeToggle/>
+      </Provider>
+    </ThemeProvider>
   );
 }
 

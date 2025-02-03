926a5ef784aaa8c5211cecaa5126bed71aeac560
gsaintskad <gsaintskad@gmail.com>
2025-02-03 12:48:20 +0100

    new structure

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller('/graph-playground-backend')
 export class AppController {
+    constructor(private appService: AppService) {
+    }
     @Get('/All')
     getAll(){
         console.log("an http request");
+        return this.appService.getAll()
     }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,23 @@
 import {Module} from "@nestjs/common";
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 @Module({
     controllers: [AppController],
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'G811282930.b',
+            database: 'Algorithmer',
+            models: [],
+            autoLoadModels: true,
+        }),
+        UsersModule,
+    ],
 })
 export class AppModule {}

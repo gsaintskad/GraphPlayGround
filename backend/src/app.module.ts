c78bccf1961faa8750adb9ce67171058d3aea36e
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-10-14 14:07:33 +0200

    tree module init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphModule } from './graph/graph.module';
+import { TreeModule } from './tree/tree.module';
 
 @Module({
-  imports: [GraphModule],
+  imports: [GraphModule, TreeModule],
   controllers: [AppController],
   providers: [AppService],
 })

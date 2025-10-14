e24f7d74b8d1e9345d7ac5cbd00654446c0c925c
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-10-14 14:45:13 +0200

    prisma init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphModule } from './graph/graph.module';
 import { TreeModule } from './tree/tree.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GraphModule, TreeModule],
+  imports: [GraphModule, TreeModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
